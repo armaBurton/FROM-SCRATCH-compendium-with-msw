a7b5ef90e34108e4eb49eda79f1cd2b076ea6c7d
armaBurton <armanarma@armaburton.com>
2022-04-29 12:58:02 -0700

    dropdown has been added

--- src/views/JokesList/JokesList.jsx
+++ src/views/JokesList/JokesList.jsx
@@ -5,13 +5,28 @@ import style from './JokesList.css';
 
 export default function JokesList() {
   const {
-    loading,
+    loading, setLoading
   } = useJokeContext();
 
   return(
     <section className={style.jokeGallery}>
       <header className={style.headSpace}>
         <h1>HARDY-HAR-HAR</h1>
+        <div>
+          <button onClick={() => {
+            setLoading(true);
+            location.href = '/';
+          }}>Gimmie Moar Jokes</button>
+          <select>
+            <option value="Random">Random</option>
+            <option value="Programming">Programming</option>
+            <option value="Misc">Misc</option>
+            <option value="Dark">Dark</option>
+            <option value="Pun">Pun</option>
+            <option value="Spooky">Spooky</option>
+            <option value="Christmas">Christmas</option>
+          </select>
+        </div>
       </header>
       <div className={style.jokesList}>
         {
